ce9beaf038c050c234e5d49d350a36cc655974d4
Ferdiansyah <ferdif9996@gmail.com>
2021-05-04 19:34:40 +0700

    create page initial

---
+++ src/page/accountactivity.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class AccountActivity extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+	render(){
+		return(
+			<p>Initial route</p>
+		)
+	}
+}
+
+export default AccountActivity
---
+++ src/page/register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Register extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+	render(){
+		return(
+			<p>Initial route</p>
+		)
+	}
+}
+
+export default Register
