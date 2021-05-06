b937ce33c99fae0b17339685838a37264f254d39
Ferdiansyah <ferdif9996@gmail.com>
2021-05-06 21:00:04 +0700

    create page explore

---
+++ src/page/explore.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Explore extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+	render(){
+		return(
+			<p>Initial route</p>
+		)
+	}
+}
+
+export default Explore
