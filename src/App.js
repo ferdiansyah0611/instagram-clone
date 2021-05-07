75166acfdb055192733732f9906f42639c8f2a27
Ferdiansyah <ferdif9996@gmail.com>
2021-05-07 20:02:11 +0700

    custom navbar on login/register pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
+  Link,
   Route,
 } from 'react-router-dom'
 // context
@@ -48,25 +49,42 @@ class App extends React.Component{
       <Router>
         <DefaultContext.Provider value={this.state}>
           <div className="App">
-            <Navbar/>
             <section className="px-3 sm:px-12 lg:px-32 mt-20 mb-20">
               <Switch>
                 <Route exact path="/">
+                  <Navbar/>
                   <Home />
                 </Route>
                 <Route path="/login">
+                  <nav class="bg-white border border-r-0 border-l-0 border-gray-300 fixed top-0 left-0 w-full z-40">
+                    <div class="flex">
+                      <div class="flex w-1/2 lg:ml-5 md:ml-14 lg:ml-32 lg:justify-start">
+                        <Link to="/" class="p-4 font-bold">Instagram Clone</Link>
+                      </div>
+                    </div>
+                  </nav>
                   <Login />
                 </Route>
                 <Route path="/register">
+                  <nav class="bg-white border border-r-0 border-l-0 border-gray-300 fixed top-0 left-0 w-full z-40">
+                    <div class="flex">
+                      <div class="flex w-1/2 lg:ml-5 md:ml-14 lg:ml-32 lg:justify-start">
+                        <Link to="/" class="p-4 font-bold">Instagram Clone</Link>
+                      </div>
+                    </div>
+                  </nav>
                   <Register />
                 </Route>
                 <Route path="/explore">
+                  <Navbar/>
                   <Explore />
                 </Route>
                 <Route path="/direct/inbox">
+                  <Navbar/>
                   <DirectInbox/>
                 </Route>
                 <Route path="/account/activity">
+                  <Navbar/>
                   <AccountActivity/>
                 </Route>
                 <Route path="/profile/:name" component={ProfileShow}/>
